4e07d771fc3b4403c78ad509fecbdc4f3665fd65
DuretiTilmo <112039625+DuretiTilmo@users.noreply.github.com>
2023-04-13 15:42:26 -0700

    added comments

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import "./App.css";
-// import Navigation from './components/Navigation';
+// importing the components
 import PortfolioContainer from "./components/PortfolioContainer";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 
 function App() {
   return (
+    // He, we have a return method that contains all the components we want to render to the screen.
     <div>
       <Header />
       <div className="container">
--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -1,24 +1,28 @@
 import React, { useState } from 'react';
+//importing our components
 import Navigation from './Navigation/index';
 import Project from './Project/index';
 import About from './pages/About';
 import Contact from './pages/Contact';
 import Resume from './pages/Resume';
 
-
+//  A state variable 'currentPage' is declared using 'useState'and then update the page by 'setCurrentPage' method
 export default function ProjectContainer() {
   const [currentPage, setCurrentPage] = useState('About');
-
+// render 'contact' page if currentPage is contact
   const renderPage = () => {
     if (currentPage === 'Contact') {
       return <Contact />;
     }
+    // render 'project' page if currentPage is project
     if (currentPage === 'Project') {
       return <Project />;
     }
+    // render 'about' page if currentPage is about
     if (currentPage === 'About') {
       return <About />;
     }
+    // render 'resume' page if currentPage is resume
     return <Resume />;
   };
 
